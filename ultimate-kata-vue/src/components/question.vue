62fc3c950e090781e8dcecc9bc11cb31a4853e58
dm-creations <d.xl.mccormack@gmail.com>
2020-03-21 11:47:12 +0000

    Basic split component testing

---
+++ ultimate-kata-vue/src/components/question.vue
@@ -0,0 +1,51 @@
+<template>
+  <div :class="[mainClass, background]">
+    <h1 :class="h1Text">{{ title }}</h1>
+    <p>
+    </p>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'question',
+  props: {
+    title: String
+  },
+  data() {
+    return {
+      mainClass: 'question',
+      background: 'code-bg',
+      h1Text: 'white-txt',
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1 {
+  margin-top: 0;
+}
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+.code-bg {
+  background-color: #212121; /* ideally {{code-bg}} */
+  height: calc(100vh - 105px);
+}
+.white-txt {
+  color: white;
+}
+</style>
---
+++ ultimate-kata-vue/src/components/render.vue
@@ -0,0 +1,50 @@
+<template>
+  <div :class="[question, background]">
+    <h1 :class="h1Text">{{ title }}</h1>
+    <p>
+    </p>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'render',
+  props: {
+    title: String
+  },
+  data() {
+    return {
+      question: 'question',
+      background: 'code-bg',
+      h1Text: 'black-txt'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1 {
+  margin-top: 0;
+}
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+.code-bg {
+  height: calc(100vh - 105px);
+}
+.black-txt {
+  color: black;
+}
+</style>
